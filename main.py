580b4377bec2a7f60c18871b6db6d6ad12ce2e57
Igor Krupkin <krupkin.ig@mail.ru>
2022-11-14 18:46:18 +0500

    set st interface

--- main.py
+++ main.py
@@ -9,13 +9,12 @@ if user_input and button :
   context = wikipedia.search(user_input)
   pipe = pipeline("question-answering", model="AlexKay/xlm-roberta-large-qa-multilingual-finedtuned-ru")
   text = wikipedia.page(context[0]).content
-  st.write(text)
   while True:
     answer_input = st.text_area('Ваш вопрос')
     answer_button = st.button('Спросить')
     exit_button = st.button('Закончить')
     if answer_button and answer_input:
       answer = pipe(question=qu, context= text)
-      print(answer["answer"])
+      st.write(answer)
     elif exit_button:
       break
