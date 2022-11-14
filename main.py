4557be4a3f58e8286e37a80704ad33b096ad4d8e
Igor Krupkin <krupkin.ig@mail.ru>
2022-11-14 19:07:33 +0500

    set st interface

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from transformers import pipeline
 import wikipedia
 import streamlit as st
 
+st.title("Поговрите с личностью из википедии!")
+
 user_input = st.text_area('С кем хотите поговорить?')
 button = st.button('Поговорить')
 
 if user_input and button:
+  print("run wikipedia search")
   context = wikipedia.search(user_input)
   pipe = pipeline("question-answering", model="AlexKay/xlm-roberta-large-qa-multilingual-finedtuned-ru")
   text = wikipedia.page(context[0]).content
+  print("got wiki page")
   answer_input = st.text_area('Ваш вопрос')
   answer_button = st.button('Спросить')
   exit_button = st.button('Закончить')
