1cc58a8e5f2c248570e7003b81caf56850b2c4ff
Igor Krupkin <krupkin.ig@mail.ru>
2022-11-15 00:45:42 +0500

    fix program for streamlit

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ from transformers import pipeline
 import wikipedia
 import streamlit as st
 
-st.title("Поговрите с личностью из википедии!")
+st.title("Поговорите с личностью из википедии!")
 
 user_input = st.text_area('С кем хотите поговорить?')
 button = st.button('Поговорить')
 
 if user_input and button:
-  print("run wikipedia search")
+  print("search wikipedia page")
+  st.session_state['pipeline'] = pipeline("question-answering",
+                                          model="AlexKay/xlm-roberta-large-qa-multilingual-finedtuned-ru")
   context = wikipedia.search(user_input)
-  pipe = pipeline("question-answering", model="AlexKay/xlm-roberta-large-qa-multilingual-finedtuned-ru")
-  text = wikipedia.page(context[0]).content
-  print("got wiki page")
-  answer_input = st.text_area('Ваш вопрос')
-  answer_button = st.button('Спросить')
-  exit_button = st.button('Закончить')
-  if answer_button and answer_input:
-    answer = pipe(question=answer_input, context= text)
-    st.write(answer)
+  st.session_state["text"] = wikipedia.page(context[0]).content
+  print("got wikipedia article")
+
+answer_input = st.text_area('Ваш вопрос')
+answer_button = st.button('Спросить')
+
+if answer_input and answer_button:
+  print("start answer search")
+  pipe = st.session_state['pipeline']
+  answer = pipe(question=answer_input, context=st.session_state["text"])
+  st.write(answer['answer'])
+  print("found answer")
+print("end of program")
