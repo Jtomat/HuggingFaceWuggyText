c35146d27eeadcf8d32a772f69e4bc5def815b8c
Igor Krupkin <krupkin.ig@mail.ru>
2022-11-14 19:01:21 +0500

    set st interface

--- main.py
+++ main.py
@@ -5,16 +5,13 @@ import streamlit as st
 user_input = st.text_area('С кем хотите поговорить?')
 button = st.button('Поговорить')
 
-if user_input and button :
+if user_input and button:
   context = wikipedia.search(user_input)
   pipe = pipeline("question-answering", model="AlexKay/xlm-roberta-large-qa-multilingual-finedtuned-ru")
   text = wikipedia.page(context[0]).content
-  while True:
-    answer_input = st.text_area('Ваш вопрос')
-    answer_button = st.button('Спросить')
-    exit_button = st.button('Закончить')
-    if answer_button and answer_input:
-      answer = pipe(question=answer_input, context= text)
-      st.write(answer)
-    elif exit_button:
-      break
+  answer_input = st.text_area('Ваш вопрос')
+  answer_button = st.button('Спросить')
+  exit_button = st.button('Закончить')
+  if answer_button and answer_input:
+    answer = pipe(question=answer_input, context= text)
+    st.write(answer)
