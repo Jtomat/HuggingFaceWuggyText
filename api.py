9bcf3c5ec553f1c5ffce8b3e20762205016cb6a5
Igor Krupkin <krupkin.ig@mail.ru>
2022-12-12 18:29:18 +0500

    set fastAPI api

---
+++ api.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from fastapi import FastAPI
+from transformers import pipeline
+import wikipedia
+from typing import Union
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    text: str
+
+
+app = FastAPI()
+classifier = pipeline("sentiment-analysis")
+pipe = pipeline("question-answering", model="AlexKay/xlm-roberta-large-qa"
+                                            "-multilingual-finedtuned-ru")
+
+
+@app.get("/")
+def root(q: str = None):
+    if q:
+        context = wikipedia.search(q)
+        print(context)
+        return {"result": "success", "data": context}
+    return {"result": "error", "message": "add 'q' parameter to request"}
+
+
+@app.get("/ask")
+def predict(title: str = None, question: str = None):
+    if title and question:
+        print(title)
+        print(question)
+        context = wikipedia.search(title)
+        print('context', context)
+        article = wikipedia.page(context[0]).content
+        answer = pipe(question=question, context=article)
+        return {"result": "success", "data": answer}
+    return {"result": "error", "message": "lost some params"}
--- main.py
+++ main.py
@@ -8,20 +8,20 @@ user_input = st.text_area('С кем хотите поговорить?')
 button = st.button('Поговорить')
 
 if user_input and button:
-  print("search wikipedia page")
-  st.session_state['pipeline'] = pipeline("question-answering",
-                                          model="AlexKay/xlm-roberta-large-qa-multilingual-finedtuned-ru")
-  context = wikipedia.search(user_input)
-  st.session_state["text"] = wikipedia.page(context[0]).content
-  print("got wikipedia article")
+    print("search wikipedia page")
+    st.session_state['pipeline'] = pipeline("question-answering",
+                                            model="AlexKay/xlm-roberta-large-qa-multilingual-finedtuned-ru")
+    context = wikipedia.search(user_input)
+    st.session_state["text"] = wikipedia.page(context[0]).content
+    print("got wikipedia article")
 
 answer_input = st.text_area('Ваш вопрос')
 answer_button = st.button('Спросить')
 
 if answer_input and answer_button:
-  print("start answer search")
-  pipe = st.session_state['pipeline']
-  answer = pipe(question=answer_input, context=st.session_state["text"])
-  st.write(answer['answer'])
-  print("found answer")
+    print("start answer search")
+    pipe = st.session_state['pipeline']
+    answer = pipe(question=answer_input, context=st.session_state["text"])
+    st.write(answer['answer'])
+    print("found answer")
 print("end of program")
